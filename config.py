3907fd96781a22a5f3d013f45ab2bcc80017b9f5
Dery Rahman A <dery.ra@gmail.com>
2018-04-22 20:29:00 +0700

    upload dir

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.environ['UPLOAD_FOLDER']
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 app.config['DEBUG'] = os.environ['DEBUG']
